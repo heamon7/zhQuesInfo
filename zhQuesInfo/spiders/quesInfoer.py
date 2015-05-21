017490317b06b3e0cc301ab271fb59efc058ad02
heamon7 <heamon7@gmail.com>
2015-05-21 21:11:25 +0800

    no

--- zhQuesInfo/spiders/quesInfoer.py
+++ zhQuesInfo/spiders/quesInfoer.py
@@ -44,7 +44,7 @@ class QuesinfoerSpider(scrapy.Spider):
 
         dbPrime = 97
         totalCount = int(client_s.get('totalCount'))
-        for questionIndex in range(0,1):
+        for questionIndex in range(0,totalCount+1):
             self.questionIdSet.add(int(client_s.get(str(questionIndex))[0]))
 
         # for tableIndex in range(dbPrime):
